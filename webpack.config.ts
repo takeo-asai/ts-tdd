c35950c39666ae92b7e7a5104c41f2f905553247
takeo asai <t_asai@interspace.ne.jp>
2017-12-14 10:38:48 +0900

    Add tslint

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,13 +3,20 @@ import * as path from 'path';
 
 const config: webpack.Configuration = {
     entry: './src/index.ts',
+    devtool: 'inline-source-map',
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
-            }
+            },
+            {
+                enforce: 'pre',
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: 'tslint-loader'
+            },
         ]
     },
     resolve: {
